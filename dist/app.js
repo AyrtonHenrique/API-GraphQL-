b47e309552e26ce726cffea8d15341492be6c057
Ayrton Henrique <ayrtonhe@live.com>
2018-09-23 11:16:24 -0300

    Configurando schema

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,18 @@
 "use strict";
 exports.__esModule = true;
 var express = require("express");
+var graphqlHTTP = require("express-graphql");
+var schema_1 = require("./graphql/schema");
 var App = /** @class */ (function () {
     function App() {
         this.express = express();
         this.middleware();
     }
     App.prototype.middleware = function () {
-        this.express.use('/hello', function (req, res, next) {
-            res.send({
-                hello: "hello world!"
-            });
-        });
+        this.express.use('/graphql', graphqlHTTP({
+            schema: schema_1["default"],
+            graphiql: process.env.NODE_ENV.trim() === 'development'
+        }));
     };
     return App;
 }());
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,8 +1,30 @@
-const typeDef = `
-  type user {
+import { makeExecutableSchema } from "graphql-tools"
+import { graphql } from "graphql";
+
+const users: any[] =[
+  {
+    id:1,
+    name: "ayrton",
+    email: "ayrton@live.com"
+  }
+];
+
+const typeDefs = `
+  type User {
     id: ID!
     name: String!
     email: String!
   }
-`
 
+  type Query {
+    allUsers: [User!]!
+  }
+`;
+
+const resolvers ={
+  Query: {
+    allUsers:() => users
+  }
+}
+
+export default makeExecutableSchema({ typeDefs, resolvers});
